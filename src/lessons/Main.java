6bda9c12ed4f53d6a022e1e78e824b4908c3b089
Akmal <akmallatifov2004@gmail.com>
2023-07-26 22:02:23 +0500

    update

---
+++ src/lessons/Main.java
@@ -0,0 +1,26 @@
+package lessons;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+    static int getMax(int[] candies) {
+        int max = Integer.MIN_VALUE;
+        for (int i : candies) if (i > max) max = i;
+        return max;
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Boolean> result = new ArrayList<Boolean>(5);
+        int[] candies = {2, 3, 5, 1, 3};
+        int extraCandies = 3;
+        for (int candy : candies) {
+            if ((candy + extraCandies) >= getMax(candies))
+                result.add(true);
+            else result.add(false);
+            String s = "a";
+        }
+    }
+}
